180a2046dcaa406782b87e8310bde23af3ca9838
xApnea <jcbruno150@gmail.com>
2020-07-25 15:01:20 -0400

    Init working express server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.us(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/../dist')));
+
+app.get('/api', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+const port = process.env.PORT || 3000;
+var server = app.listen(port, () => {console.log(`Listening at http://localhost:${port}`)});
+
+module.exports = server;
