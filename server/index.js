d54fff64d8afea851d0ceffccb168fae6005b47d
xApnea <jcbruno150@gmail.com>
2020-07-27 14:35:56 -0400

    Built out basic endpoints

--- server/index.js
+++ server/index.js
@@ -11,7 +11,45 @@ app.get('/api', (req, res) => {
   res.status(200).send('Request was good');
 })
 
+//////////////////////////////////////////////////////////////////////////////////
+// USER
+//////////////////////////////////////////////////////////////////////////////////
+
+//get user
+app.get('/api/user', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+//create a new user
+app.post('/api/user', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+//////////////////////////////////////////////////////////////////////////////////
+// PROFILE PICTURE
+//////////////////////////////////////////////////////////////////////////////////
+
+//Add a new profile picture
+app.put('/api/profile', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+//////////////////////////////////////////////////////////////////////////////////
+// AUDIO
+//////////////////////////////////////////////////////////////////////////////////
+
+//Add a new audio track
+app.post('/api/audio', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+//Add a new audio track
+app.delete('/api/audio', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+
 const port = process.env.PORT || 3000;
-var server = app.listen(port, () => {console.log(`Listening at http://localhost:${port}`)});
+var server = app.listen(port, () => {console.log(`Listening on port: ${port}`)});
 
 module.exports = server;
