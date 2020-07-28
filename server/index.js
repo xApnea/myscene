f9897cd3280bc5dd03f1d36bd9f55dac03b3a4c1
xApnea <jcbruno150@gmail.com>
2020-07-28 10:27:09 -0400

    Uploads new avatar images to S3

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const AWS = require('aws-sdk');
+const multer = require('multer');
 
 const app = express();
+
+const upload = multer({storage: multer.memoryStorage()});
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/../dist')));
@@ -17,12 +21,12 @@ app.get('/api', (req, res) => {
 
 //get user
 app.get('/api/user', (req, res) => {
-  res.status(200).send('Request was good');
+  res.status(200).send('User successfully retreived');
 })
 
 //create a new user
 app.post('/api/user', (req, res) => {
-  res.status(200).send('Request was good');
+  res.status(200).send('User was successfully created');
 })
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -30,8 +34,29 @@ app.post('/api/user', (req, res) => {
 //////////////////////////////////////////////////////////////////////////////////
 
 //Add a new profile picture
-app.put('/api/profile', (req, res) => {
-  res.status(200).send('Request was good');
+app.post('/api/profile', upload.single('avatar'), (req, res) => {
+  AWS.config.loadFromPath('./config.json');
+  // var key = new Date();
+  // key = JSON.stringify(key);
+
+  console.log(req.headers);
+  console.log(req.file);
+  console.log(req.body);
+
+  var objectParams = {Bucket: 'myscene', Key: req.file.originalname, Body: req.file.buffer};
+  // Create object upload promise
+  var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+  uploadPromise
+  .then((data) => {
+      console.log(data);
+      console.log(`Successfully uploaded data to ${objectParams.Bucket}/${objectParams.Key}`);;
+      res.status(200).send(`Successfully uploaded data to ${objectParams.Bucket}/${objectParams.Key}`);
+    })
+  .catch((error) => {
+    console.log(`This is the failing field: ${error.field}`);
+    console.error(error);
+    res.status(500).send(error);
+  });
 })
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -40,14 +65,22 @@ app.put('/api/profile', (req, res) => {
 
 //Add a new audio track
 app.post('/api/audio', (req, res) => {
-  res.status(200).send('Request was good');
+  res.status(200).send('Audio request was good');
 })
 
-//Add a new audio track
+//Delete an audio track
 app.delete('/api/audio', (req, res) => {
-  res.status(200).send('Request was good');
+  res.status(200).send('Audio request was good');
 })
 
+
+//////////////////////////////////////////////////////////////////////////////////
+// AUDIO
+//////////////////////////////////////////////////////////////////////////////////
+//Change youtube video
+app.put('/api/video', (req, res) => {
+  res.status(200).send('Video request was good');
+})
 
 const port = process.env.PORT || 3000;
 var server = app.listen(port, () => {console.log(`Listening on port: ${port}`)});
